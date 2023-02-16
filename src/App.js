bde2ea5d6aba3c6ba497f438bc33fb4217a11d61
kannoufmounir <mounirkannouf@gmail.com>
2023-02-16 09:52:55 +0100

    code source

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+
+
 import './App.css';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import image1 from './1.jpg';
+import image2 from './2.jpg';
+import image3 from './3.jpg';
+import image4 from './4.jpg';
+import image5 from './5.jpg';
 
 function App() {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+
+
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Carousel responsive={responsive}>
+        <div className='img-produ'>
+          <img className='product-image' src={image1} alt="" />
+          <h1>Tommy Hilfiger </h1>
+          <h2>à partir de € 118,96 <h6>TVA incluse</h6></h2>
+          <p>MODERN MIX RUNNER - Baskets basses</p>
+          <button> Ajouter au panier </button>
+
+        </div>
+        <div className='img-produ'>
+          <img className='product-image' src={image2} alt="" />
+          <h1>Tommy Hilfiger </h1>
+          <h3>€ 47,95 TVA incluse</h3>
+          <p>CORE LIGHTWEIGHT - Baskets basses</p>
+          <button> Ajouter au panier </button>
+
+        </div>
+        <div className='img-produ'>
+          <img className='product-image' src={image3} alt="" />
+          <h1>Tommy Hilfiger </h1>
+          <h3>€ 77,93 TVA incluse</h3>
+          <p>RETRO COURT CUPSOLE STRIPES - Baskets</p>
+          <button> Ajouter au panier </button>
+
+        </div>
+        <div className='img-produ'>
+          <img className='product-image' src={image4} alt="" />
+          <h1>Tommy Hilfiger </h1>
+          <h3>€ 69,95 TVA incluse</h3>
+          <p>ICONIC LOGO - Baskets basses</p>
+          <button> Ajouter au panier </button>
+
+        </div>
+        <div className='img-produ'>
+          <img className='product-image' src={image5} alt="" />
+          <h1>Tommy Hilfiger </h1>
+          <h3>€ 98,96 TVA incluse</h3>
+          <p>ICONIC RUNNER - Baskets basses</p>
+          <button> Ajouter au panier </button>
+
+        </div>
+      </Carousel>
+
     </div>
   );
 }
